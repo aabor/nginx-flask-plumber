b5592764f6d54d605423b4b16e3dc07e6aa651a1
AlexBorochkin <aaborochkin@gmail.com>
2019-01-28 21:48:35 +0300

    tests improved

--- rnews/tests/run-tests.R
+++ rnews/tests/run-tests.R
@@ -1,3 +1,3 @@
 library('testthat')
 options(testthat.output_file = "test-reports/rnews.xml")
-testthat::test_dir('rnews/tests/testthat/')
+testthat::test_dir('rnews/tests/testthat')
--- rnews/tests/testthat/test_api.R
+++ rnews/tests/testthat/test_api.R
@@ -2,26 +2,27 @@ library(tidyverse)
 library(readr)
 library(jsonlite)
 library("httr")
-#library(logging)
 library(testthat)
 
-context("rnews Testing Succeeded!")
-test_that("Multipilation works ", {
-  res <- 5 * 2
-  expect_equal(res, 10)
+url<-"http://rnews:5000"
+context("Main page does not exist")
+test_that("page does not exist", {
+  resp<-GET(url, path="not exist")
+  expect_equal(resp$status_code, 200)
+  ctt<-content(resp) %>% unlist %>% str_squish()
+  expect_equal(ctt, "Service rnews (language R) is ready!")
+  print("page does not exist")
 })
-
-#library(plumber)
 context("Main page exist")
-test_that("Main page exist ", {
-  resp<-GET("http://localhost", path = "rnews")
+test_that("Main page exist", {
+  resp<-GET(url)
   expect_equal(resp$status_code, 200)
   ctt<-content(resp) %>% unlist %>% str_squish()
   expect_equal(ctt, "Service rnews (language R) is ready!")
 })
 context("Health check")
-test_that("Healthy ", {
-  resp<-GET("http://localhost", path = "rnews/health")
+test_that("Health check", {
+  resp<-GET(url, path = "health")
   expect_equal(resp$status_code, 200)
   ctt<-content(resp) %>% unlist %>% str_squish()
   expect_equal(ctt, 'Healthy')
@@ -29,15 +30,15 @@ test_that("Healthy ", {
 context("Echo response exists")
 test_that("Echo response exists", {
   msg<-"rnews echo test"
-  resp<-GET("http://localhost", path = "rnews/echo", query = list(msg = msg))
+  resp<-GET(url, path = "echo", query = list(msg = msg))
   expect_equal(resp$status_code, 200)
   ctt<-content(resp) %>% unlist %>% fromJSON()
   expect_equal(ctt$msg, 'The message is: \"rnews echo test\"')
 })
 context("Pause functions")
-test_that("Pause functions ", {
+test_that("Pause functions", {
   duration<-2
-  resp<-GET("http://localhost", path = "rnews/pause", query = list(duration = duration))
+  resp<-GET(url, path = "pause", query = list(duration = duration))
   expect_equal(resp$status_code, 200)
   ctt<-content(resp) %>% unlist %>% fromJSON()
   expect_equal(ctt$msg, 'Pause of 2 seconds finished')
