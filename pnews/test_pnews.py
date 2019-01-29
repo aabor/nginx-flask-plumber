d0a330b123bf4fe896b120bf86638ec6f3be6b50
AlexBorochkin <aaborochkin@gmail.com>
2019-01-29 11:20:03 +0300

    first real test in pytest passed

--- pnews/test_pnews.py
+++ pnews/test_pnews.py
@@ -1,5 +1,6 @@
-import sys, os
-import logging
+import sys
+import os
+import shutil
 import json
 import pandas as pd
 import numpy as np
@@ -16,39 +17,21 @@ from pytz import timezone
 import pytz
 import pause
 import humanfriendly
-from tabulate import tabulate
 from bs4 import BeautifulSoup
+from tabulate import tabulate
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, url_for
 from flask import Flask, session
 from flask_session import Session
+import requests
 
 url="http://pnews:5000"
 def test_main_page():
   r = requests.get(url)
   assert r.status_code == requests.codes.ok
-  assert r.text == 'Service pnews (language python) is ready!'
-
-# class SimpleTest(unittest.TestCase):
-#     @unittest.skip("demonstrating skipping")
-#     def test_skipped(self):
-#         self.fail("shouldn't happen")
-
-#     def test_pass_assert_equal(self):
-#         self.assertEqual(10, 7 + 3)
-
-#     def test_pass_not_equal(self):
-#         self.assertNotEqual(11, 7 + 3)
-
-    # def test_fail(self):
-    #     self.assertEqual(11, 7 + 3)
-
-
-# if __name__ == '__main__':
-#     import xmlrunner
-#     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+  assert r.text.strip() == 'Service pnews (language python) is ready!'
